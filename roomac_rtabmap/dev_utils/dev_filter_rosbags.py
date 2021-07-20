d4832f7dc73511c3eee00ccc3ebc366fc8679bd4
Maciej Stepien <maciek1284@outlook.com>
2021-07-20 11:41:24 +0000

    Add utilities for working on rosbags with rtabmap

---
+++ roomac_rtabmap/dev_utils/dev_filter_rosbags.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+import rosbag
+
+
+def filterRosbag(inputRosbag, outputRosbag):
+    topics_to_save = [
+        "/camera/depth_registered/image_raw",
+        "/camera/rgb/camera_info",
+        "/camera/rgb/image_rect_color",
+        "/odom",
+    ]
+
+    with rosbag.Bag(outputRosbag, "w") as outbag:
+        for topic, msg, t in rosbag.Bag(inputRosbag).read_messages():
+            if topic == "/tf":
+                if msg.transforms[0].header.frame_id != "map":
+                    outbag.write(topic, msg, t)
+
+            elif topic in topics_to_save:
+                outbag.write(topic, msg, t)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        filterRosbag(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 2:
+        outputRosbag = str(sys.argv[1]) + "_filtered"
+        print("Output rosbag not specified. Defaulting to: " + outputRosbag)
+        filterRosbag(sys.argv[1], outputRosbag)
+    else:
+        print(
+            "Invalid number of arguments. Usage: rosrun ... filter_rosbag.py inputRosbag [ouputRosbag]"
+        )
