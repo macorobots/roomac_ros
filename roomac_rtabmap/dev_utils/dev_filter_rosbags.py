1c4a4887c64bbf45e02e99553c213a53e9e0dfcc
Maciej Stepien <maciek1284@outlook.com>
2021-08-03 09:07:30 +0000

    [Filter rosbags] Add odom msg from tf and imu msg

--- roomac_rtabmap/dev_utils/dev_filter_rosbags.py
+++ roomac_rtabmap/dev_utils/dev_filter_rosbags.py
@@ -3,6 +3,13 @@
 import sys
 import rosbag
 
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3, Pose2D
+from tf.transformations import euler_from_quaternion
+
+import math
+import copy
+
 
 def filterRosbag(inputRosbag, outputRosbag):
     topics_to_save = [
@@ -13,7 +20,14 @@ def filterRosbag(inputRosbag, outputRosbag):
         "/odom",
     ]
 
-    save_odom_tf = True
+    save_odom_tf = False
+    save_odom_topic = True
+
+    currentTime = 0
+    lastTime = 0
+
+    lastPosition = Pose2D()
+    currentPosition = Pose2D()
 
     with rosbag.Bag(outputRosbag, "w") as outbag:
         for topic, msg, t in rosbag.Bag(inputRosbag).read_messages():
@@ -24,7 +38,50 @@ def filterRosbag(inputRosbag, outputRosbag):
                     else: 
                         pass
 
+                    if save_odom_topic:
+                        odomBaseTransform = msg.transforms[0]
 
+                        currentTime = odomBaseTransform.header.stamp
+                        if lastTime == 0:
+                            lastTime = currentTime
+
+                        currentPosition.x = odomBaseTransform.transform.translation.x
+                        currentPosition.y = odomBaseTransform.transform.translation.y
+                        quaternion = [odomBaseTransform.transform.rotation.x, odomBaseTransform.transform.rotation.y, odomBaseTransform.transform.rotation.z, odomBaseTransform.transform.rotation.w]
+
+                        eulerAngles = euler_from_quaternion(quaternion)
+                        currentPosition.theta = eulerAngles[2]
+
+                        dt = (currentTime - lastTime).to_sec()
+                        if dt > 0:
+                            vx = math.sqrt((currentPosition.x - lastPosition.x)**2 + (currentPosition.y - lastPosition.y)**2)/dt
+                            vth = (currentPosition.theta - lastPosition.theta)/dt
+                        else:
+                            vx = 0
+                            vth = 0
+
+                        lastPosition = copy.deepcopy(currentPosition)
+
+                        odomMsg = Odometry()
+                        odomMsg.header.stamp = currentTime
+                        odomMsg.header.frame_id = "odom"
+                        odomMsg.child_frame_id = "base_link"
+                        odomMsg.pose.pose = Pose(Point(odomBaseTransform.transform.translation.x, odomBaseTransform.transform.translation.y, odomBaseTransform.transform.translation.z), Quaternion(quaternion[0], quaternion[1], quaternion[2], quaternion[3]))
+                        odomMsg.twist.twist = Twist(Vector3(vx, 0, 0), Vector3(0, 0, vth))
+
+                        lastTime = currentTime
+
+                        outbag.write("/odom", odomMsg, t)
+
+            elif topic == "/android/imu":
+                msg.header.frame_id = "imu"
+                msg.header.stamp = t
+                msg.orientation_covariance  = list(msg.orientation_covariance)
+                msg.orientation_covariance[0] = 0.001
+                msg.orientation_covariance[4] = 0.001
+                msg.orientation_covariance[8] = 0.001
+                msg.orientation_covariance = tuple(msg.orientation_covariance)
+                outbag.write(topic, msg, t)
             elif topic in topics_to_save:
                 outbag.write(topic, msg, t)
 
