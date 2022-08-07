a3742c6d424dd6ca2a702f5d120cc181f9c32abe
Maciej Stepien <maciek1284@outlook.com>
2022-08-07 13:18:23 +0000

    Update using gripper in real picking manager

--- roomac_autonomous_manipulation/scripts/real_picking_object_manager.py
+++ roomac_autonomous_manipulation/scripts/real_picking_object_manager.py
@@ -22,9 +22,7 @@ class RealPickingObjectManager(PickingObjectManager):
 
         # Parameters
         object_type = rospy.get_param("~object_type", "bottle")
-        self.gripper_joint_name = rospy.get_param(
-            "~gripper_joint_name", "right_gripper_joint"
-        )
+
         self.opened_gripper_value = rospy.get_param("~opened_gripper_value", 1.0)
         self.bottle_closed_gripper_value = rospy.get_param(
             "~bottle_closed_gripper_value", 0.4
@@ -33,54 +31,11 @@ class RealPickingObjectManager(PickingObjectManager):
             "~cardbox_object_closed_gripper_value", 0.1
         )
 
-        self.object_point = Point()
-
-        # object is positioned away from ar tag + componsetion for
-        # difference between real manipulator and model
-
-        if object_type == "bottle":
-            self.object_point.x = -0.06 - 0.055
-            self.object_point.y = -0.16 - 0.008 + 0.04
-            self.object_point.z = 0.195 + 0.02
-        elif object_type == "cardbox_object":
-            self.object_point.x = -0.04 - 0.025
-            self.object_point.y = -0.09 - 0.008 + 0.02
-            self.object_point.z = 0.1
-
-        self.closed_gripper_value = 0.1
+        self.closed_gripper_value = -0.013
         if object_type == "bottle":
             self.closed_gripper_value = self.bottle_closed_gripper_value
         elif object_type == "cardbox_object":
             self.closed_gripper_value = self.cardbox_object_closed_gripper_value
-
-        self.follow_joint_trajectory_client = actionlib.SimpleActionClient(
-            "/arm_position_controller/follow_joint_trajectory",
-            FollowJointTrajectoryAction,
-        )
-        self.follow_joint_trajectory_client.wait_for_server()
-
-    def move_gripper(self, position, delay=1.0):
-        point = JointTrajectoryPoint()
-        point.positions = [position]
-        point.time_from_start = rospy.Duration(delay)
-
-        goal = FollowJointTrajectoryGoal()
-        goal.trajectory.header.stamp = rospy.Time.now()
-        goal.trajectory.header.frame_id = "base_link"
-        goal.trajectory.joint_names = [self.gripper_joint_name]
-        goal.trajectory.points = [point]
-
-        self.follow_joint_trajectory_client.send_goal(goal)
-
-        rospy.sleep(delay)
-
-    def close_gripper(self, delay=1.0):
-        rospy.loginfo("Sending close gripper command")
-        self.move_gripper(self.closed_gripper_value, delay)
-
-    def open_gripper(self, delay=1.0):
-        rospy.loginfo("Sending open gripper command")
-        self.move_gripper(self.opened_gripper_value, delay)
 
 
 if __name__ == "__main__":
