8b671d4b4fc6c0d1c2b91033fbaf641a101cc5ea
Maciej Stepien <maciek1284@outlook.com>
2022-09-01 21:07:32 +0000

    Refactor publishing odom msgs

--- roomac_rtabmap/scripts/odom_tf_to_topic.py
+++ roomac_rtabmap/scripts/odom_tf_to_topic.py
@@ -1,61 +1,95 @@
 #!/usr/bin/env python
 
-import rospy
-import tf2_ros
-from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3, Pose2D
-from tf.transformations import euler_from_quaternion
-
 import math
 import copy
 
-rospy.init_node('odometry_topic_publisher')
-odomPub = rospy.Publisher("odom", Odometry, queue_size=50)
+import rospy
+import tf2_ros
 
-currentTime = rospy.Time.now()
-lastTime = rospy.Time.now()
+from tf.transformations import euler_from_quaternion
 
-lastPosition = Pose2D()
-currentPosition = Pose2D()
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3, Pose2D
 
-tfBuffer = tf2_ros.Buffer()
-listener = tf2_ros.TransformListener(tfBuffer)
 
-rate = rospy.Rate(20.0)
+class OdomTfToTopic:
+    def __init__(self):
 
-while not rospy.is_shutdown():
-    try:
-        odomBaseTransform = tfBuffer.lookup_transform('odom', 'base_link', rospy.Time(0))
-    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-        rate.sleep()
-        continue
+        self.odom_pub = rospy.Publisher("odom", Odometry, queue_size=50)
 
-    currentTime = odomBaseTransform.header.stamp
+        self.last_time = rospy.Time.now()
+        self.last_position = Pose2D()
 
-    currentPosition.x = odomBaseTransform.transform.translation.x
-    currentPosition.y = odomBaseTransform.transform.translation.y
-    quaternion = [odomBaseTransform.transform.rotation.x, odomBaseTransform.transform.rotation.y, odomBaseTransform.transform.rotation.z, odomBaseTransform.transform.rotation.w]
+        self.tf_buffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tf_buffer)
 
-    eulerAngles = euler_from_quaternion(quaternion)
-    currentPosition.theta = eulerAngles[2]
+        self.rate = rospy.Rate(20.0)
 
-    dt = (currentTime - lastTime).to_sec()
-    if dt > 0:
-        vx = math.sqrt((currentPosition.x - lastPosition.x)**2 + (currentPosition.y - lastPosition.y)**2)/dt
-        vth = (currentPosition.theta - lastPosition.theta)/dt
-    else:
-        vx = 0
-        vth = 0
+    def run(self):
+        while not rospy.is_shutdown():
+            try:
+                odom_base_transform = self.tf_buffer.lookup_transform(
+                    "odom", "base_link", rospy.Time(0)
+                )
+            except (
+                tf2_ros.LookupException,
+                tf2_ros.ConnectivityException,
+                tf2_ros.ExtrapolationException,
+            ):
+                self.rate.sleep()
+                continue
 
-    lastPosition = copy.deepcopy(currentPosition)
+            current_time = odom_base_transform.header.stamp
 
-    odomMsg = Odometry()
-    odomMsg.header.stamp = currentTime
-    odomMsg.header.frame_id = "odom"
-    odomMsg.child_frame_id = "base_link"
-    odomMsg.pose.pose = Pose(Point(odomBaseTransform.transform.translation.x, odomBaseTransform.transform.translation.y, odomBaseTransform.transform.translation.z), Quaternion(quaternion[0], quaternion[1], quaternion[2], quaternion[3]))
-    odomMsg.twist.twist = Twist(Vector3(vx, 0, 0), Vector3(0, 0, vth))
-    odomPub.publish(odomMsg)
+            current_position = Pose2D()
+            current_position.x = odom_base_transform.transform.translation.x
+            current_position.y = odom_base_transform.transform.translation.y
+            quaternion = [
+                odom_base_transform.transform.rotation.x,
+                odom_base_transform.transform.rotation.y,
+                odom_base_transform.transform.rotation.z,
+                odom_base_transform.transform.rotation.w,
+            ]
 
-    lastTime = currentTime
-    rate.sleep()
+            euler_angles = euler_from_quaternion(quaternion)
+            current_position.theta = euler_angles[2]
+
+            dt = (current_time - self.last_time).to_sec()
+            if dt > 0:
+                vx = (
+                    math.sqrt(
+                        (current_position.x - self.last_position.x) ** 2
+                        + (current_position.y - self.last_position.y) ** 2
+                    )
+                    / dt
+                )
+                vth = (current_position.theta - self.last_position.theta) / dt
+            else:
+                vx = 0
+                vth = 0
+
+            self.last_position = copy.deepcopy(current_position)
+
+            odom_msg = Odometry()
+            odom_msg.header.stamp = current_time
+            odom_msg.header.frame_id = "odom"
+            odom_msg.child_frame_id = "base_link"
+            odom_msg.pose.pose = Pose(
+                Point(
+                    odom_base_transform.transform.translation.x,
+                    odom_base_transform.transform.translation.y,
+                    odom_base_transform.transform.translation.z,
+                ),
+                Quaternion(quaternion[0], quaternion[1], quaternion[2], quaternion[3]),
+            )
+            odom_msg.twist.twist = Twist(Vector3(vx, 0, 0), Vector3(0, 0, vth))
+            self.odom_pub.publish(odom_msg)
+
+            self.last_time = current_time
+            self.rate.sleep()
+
+
+if __name__ == "__main__":
+    rospy.init_node("odometry_topic_publisher")
+    odom_publisher = OdomTfToTopic()
+    odom_publisher.run()
