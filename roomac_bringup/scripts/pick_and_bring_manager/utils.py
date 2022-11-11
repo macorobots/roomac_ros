2ab7b75913c44b0e27051e1862bf69aea784ca2a
Maciej Stepien <maciek1284@outlook.com>
2022-11-11 10:09:26 +0000

    Add checking artag distance from camera

--- roomac_bringup/scripts/pick_and_bring_manager/utils.py
+++ roomac_bringup/scripts/pick_and_bring_manager/utils.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python
 
+import math
+
 import rospy
 
 import tf2_ros
 from tf.transformations import euler_from_quaternion
 
-from geometry_msgs.msg import Pose2D
 
-
-def get_latest_position_from_transform(target_frame, source_frame, tf_buffer):
-    """Gets the most recent transform from source_frame to target_frame and converts it
-    to Pose2D msg using x,y and yaw
+def get_latest_transform(target_frame, source_frame, tf_buffer):
+    """Returns the most recent transform from source_frame to target_frame
 
     Raises:
         RuntimeError: when transform doesn't exist
     """
 
     try:
-        current_transform = tf_buffer.lookup_transform(
-            target_frame, source_frame, rospy.Time(0)
-        )
+        return tf_buffer.lookup_transform(target_frame, source_frame, rospy.Time(0))
     except (
         tf2_ros.LookupException,
         tf2_ros.ConnectivityException,
@@ -29,17 +26,22 @@ def get_latest_position_from_transform(target_frame, source_frame, tf_buffer):
             "Couldn't get " + target_frame + "->" + source_frame + " transform"
         )
 
-    current_position = Pose2D()
-    current_position.x = current_transform.transform.translation.x
-    current_position.y = current_transform.transform.translation.y
-    quaternion = [
-        current_transform.transform.rotation.x,
-        current_transform.transform.rotation.y,
-        current_transform.transform.rotation.z,
-        current_transform.transform.rotation.w,
-    ]
 
-    eulerAngles = euler_from_quaternion(quaternion)
-    current_position.theta = eulerAngles[2]
+def get_yaw_angle(quaternion):
+    """Calculates yaw euler angle from given quaternion"""
 
-    return current_position
+    euler_angles = euler_from_quaternion(
+        [
+            quaternion.x,
+            quaternion.y,
+            quaternion.z,
+            quaternion.w,
+        ]
+    )
+    return euler_angles[2]
+
+
+def get_distance_between_vectors(vec1, vec2):
+    return math.sqrt(
+        (vec2.x - vec1.x) ** 2 + (vec2.y - vec1.y) ** 2 + (vec2.z - vec1.z) ** 2
+    )
